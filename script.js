1d08f057a2112e63ddf411fe7b6a9ff3fb3c0247
samuray90 <samuray_90@yahoo.com>
2020-06-12 13:32:42 +0100

    :when you place a list of numbers make sure you declaire your object by plural

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
-const colorBtn = document.querySelector('.colorBtn');
+const hexNumbers = [0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F'];
+const hexBtn = document.querySelector('.hexBtn');
 const bodyBcg = document.querySelector('body');
+const hex = document.querySelector('.hex');
 
-const colors = ['yellow','red','blue'];
+hexBtn.addEventListener('click',getHex);
 
-colorBtn.addEventListener('click',changeColor);
+function getHex (){
+let hexCol = '#';
 
-function changeColor (){
-    // bodyBcg.style.backgroundColor = colors[2];
-    let random = Math.floor(Math.radndom()*colors.length)
-    bodyBcg.style.backgroundColor = colors[random];
+for(let i = 0; i<6; i++){
+let random = Math.floor(Math.random()*hexNumbers.length);
+hexCol +=hexNumbers[random];
+}
+
+bodyBcg.style.backgroundColor = hexCol;
+hex.innerHTML = hexCol;
+
 }
