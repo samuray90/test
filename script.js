a1af372886fc0811a048ad084a4d8d8ab23dfb68
samuray90 <samuray_90@yahoo.com>
2020-06-12 15:56:11 +0100

    :make sure when you produce a nr of events to stick to precise wording

---
+++ script.js
@@ -0,0 +1,41 @@
+const quotes = [
+  {
+      name:'Johnny daniel',
+      quote:'get busy coding'
+  },
+  {
+      name:'John Kenedy',
+      quote:'those who dare to fail miserably  can achieve greatly'
+  },
+  {
+      name:'Abraham LIncoln',
+      quote:'I`m a success today because I had a friend who believed in me and I didn`t havethe heart to let him down'
+  }
+
+]
+
+const quoteBtn = document.querySelector('#quoteBtn');
+const quoteAuthor = document.querySelector('#quoteAuthor');
+const quote = document.querySelector('#quote');
+
+quoteBtn.addEventListener('click', displayQuote);
+
+function displayQuote (){
+
+let number = Math.floor(Math.random()*quotes.length);
+quoteAuthor.innerHTML = quotes[number].name;
+quote.innerHTML = quotes[number].quote;
+}
+
+
+
+// const simpleQuotes = [
+//   {
+//     name:'author number 1',
+//     quote:'quote number 1'
+//   },
+//   {
+//     name:'author number 2',
+//     quote:'quote number 2'
+//   }
+// ]
